d7bd156bfd064e7b15a6813d18269ead7aff8231
Zylyndro <zyad.elnemr@bulme.at>
2022-01-26 21:04:22 +0100

    done the work

--- webclient.cpp
+++ webclient.cpp
@@ -1,7 +1,48 @@
 #include "webclient.h"
+#include <QDebug>
 
 Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    connect(input, &QLineEdit::returnPressed, this, &Webclient::on_gobtn_clicked);
+
+    m_socket = new QTcpSocket(this);
+    connect(m_socket, &QTcpSocket::connected, this, &Webclient::socketConnected);
+    connect(m_socket, &QTcpSocket::readyRead, this, &Webclient::printAndDisconnect);
+}
+
+void Webclient::outputMessage(QString message, QColor color)
+{
+    outputterminal->clear();
+    outputterminal->setTextColor(color);
+    outputterminal->setText(message);
+}
+
+void Webclient::socketConnected()
+{
+
+    m_socket->write(QString("GET / HTTP/1.1\r\nHost:" + m_search + "\r\n\r\n").toLocal8Bit());
+
+}
+
+void Webclient::printAndDisconnect()
+{
+    qDebug()<<"connected";
+    outputMessage(m_socket->readAll());
+    m_socket->disconnectFromHost();
+}
+
+void Webclient::on_gobtn_clicked()
+{
+    //m_socket->reset();
+    if(input->text().isEmpty())
+    {
+        outputMessage("No such adress found");
+    }
+    else
+    {
+        m_socket->connectToHost(input->text(), m_port);
+    }
 }
--- webclient.h
+++ webclient.h
@@ -2,6 +2,9 @@
 #define WEBCLIENT_H
 
 #include "ui_webclient.h"
+#include <QDebug>
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class Webclient : public QWidget, private Ui::Webclient
 {
@@ -9,6 +12,19 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void on_gobtn_clicked();
+
+private:
+    void outputMessage(QString message, QColor color = QColor::fromRgb(255, 0, 255));
+    void socketConnected();
+    void printAndDisconnect();
+
+private:
+    QTcpSocket *m_socket = nullptr;
+    QString m_search;
+    const char m_port = 80;
 };
 
 #endif // WEBCLIENT_H
